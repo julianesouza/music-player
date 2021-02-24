49a92fede28641babb89ee8c05c22381d9041df4
Juliane Souza <julianesouza.pro@gmail.com>
2021-02-24 18:36:23 -0400

    audio refs

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 //Adding components
 import Player from './components/Player';
 import Song from './components/Song';
+import './styles/app.scss';
 
+import data from './util';
 
 function App() {
+
+  const [songs, setSongs] = useState(data());
+  const [currentSong, setCurrentSong] = useState(songs[0]);
+  console.log(songs);
+  console.log(currentSong);
+
   return (
     <div className="App">
-      <Song />
-      <Player />
+      <Song currentSong={currentSong} />
+      <Player currentSong={currentSong}/>
     </div>
   );
 }
--- src/components/Player.js
+++ src/components/Player.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPlay, faAngleLeft, faAngleRight } from '@fortawesome/free-solid-svg-icons'
 
 
-const Player = () => {
+const Player = ({ currentSong }) => {
+
+    //Refs
+    const audioRef = useRef(null);
+
+    const playSongHandler = () => {
+        console.log(audioRef.current);
+    }
+
     return(
         <div className="player">
             <div className="time-control">
@@ -14,9 +22,10 @@ const Player = () => {
 
             <div className="play-control">
                 <FontAwesomeIcon className="back" size="2x" icon={faAngleLeft}/>
-                <FontAwesomeIcon className="play" size="2x" icon={faPlay}/>
+                <FontAwesomeIcon onClick={playSongHandler} className="play" size="2x" icon={faPlay}/>
                 <FontAwesomeIcon className="forward" size="2x" icon={faAngleRight}/>
             </div>
+            <audio ref={audioRef} src={currentSong.audio}></audio>
         </div>
     );
 }
