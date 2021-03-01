ca48cfc5f55e0b06e2fd586df57c3ca1c8bd6f29
Juliane Souza <julianesouza.pro@gmail.com>
2021-03-01 18:02:31 -0400

    setting player states

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 //Adding components
 import Player from './components/Player';
 import Song from './components/Song';
-import './styles/app.scss';
+import './styles/app.scss'; //importing styles
 
 import data from './util';
 
@@ -10,13 +10,16 @@ function App() {
 
   const [songs, setSongs] = useState(data());
   const [currentSong, setCurrentSong] = useState(songs[0]);
-  console.log(songs);
-  console.log(currentSong);
+  const [isPlaying, setIsPlaying] = useState(false);
 
   return (
     <div className="App">
       <Song currentSong={currentSong} />
-      <Player currentSong={currentSong}/>
+      <Player 
+        currentSong={currentSong}
+        isPlaying={isPlaying}
+        setIsPlaying={setIsPlaying}
+      />
     </div>
   );
 }
--- src/components/Player.js
+++ src/components/Player.js
@@ -3,13 +3,19 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPlay, faAngleLeft, faAngleRight } from '@fortawesome/free-solid-svg-icons'
 
 
-const Player = ({ currentSong }) => {
+const Player = ({ currentSong, isPlaying, setIsPlaying }) => {
 
     //Refs
     const audioRef = useRef(null);
 
     const playSongHandler = () => {
-        console.log(audioRef.current);
+        if(isPlaying){
+            audioRef.current.pause();
+            setIsPlaying(!isPlaying);
+        }else{
+            audioRef.current.play();
+            setIsPlaying(!isPlaying);
+        }
     }
 
     return(
